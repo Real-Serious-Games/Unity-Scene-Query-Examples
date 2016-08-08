5f9dd2cd95ac03802a8177825b0054dc5d8130b2
Ashley Davis <ashley.davis@realseriousgames.com>
2016-08-08 16:37:30 +1000

    Example 2.

---
+++ Assets/2. Find objects by name/Find_objects_by_name.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Find_objects_by_name : MonoBehaviour {
+
+	void Start ()
+    {
+        var pedestrians = GameObject.FindObjectsOfType<GameObject>()
+            .Cast<GameObject>()
+            .Where(gameObject => gameObject.name == "Pedestrian")
+            .ToArray();
+
+        foreach (var pedestrian in pedestrians)
+        {
+            Debug.Log("Found game object " + pedestrian.name);
+        }
+    }
+}
