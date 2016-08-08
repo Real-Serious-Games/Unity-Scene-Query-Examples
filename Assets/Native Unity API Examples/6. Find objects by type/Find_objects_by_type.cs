6cfcbd242ef9c3f24a5f51c395ff241950d59abe
Ashley Davis <ashley.davis@realseriousgames.com>
2016-08-08 17:11:28 +1000

    Example 6.

---
+++ Assets/Native Unity API Examples/6. Find objects by type/Find_objects_by_type.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Find_objects_by_type : MonoBehaviour
+{
+    void Start()
+    {
+        var pedestrians = GameObject.FindObjectsOfType<Pedestrian>();
+        foreach (var pedestrian in pedestrians)
+        {
+            Debug.Log("Found pedestrian: " + pedestrian.name);
+        }
+    }
+}
