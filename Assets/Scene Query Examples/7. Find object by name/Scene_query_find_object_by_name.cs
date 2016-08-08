7667e48dbd3c3e91dbba04ff1f10092434974697
Ashley Davis <ashley.davis@realseriousgames.com>
2016-08-09 09:51:38 +1000

    Example 7.

---
+++ Assets/Scene Query Examples/7. Find object by name/Scene_query_find_object_by_name.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using RSG.Scene.Query;
+
+public class Scene_query_find_object_by_name : MonoBehaviour {
+
+	void Start ()
+    {
+        var sceneQuery = new SceneQuery();
+
+        var player = sceneQuery.SelectOne("Player");
+        Debug.Log("Found game object " + player.name);
+	}
+}
