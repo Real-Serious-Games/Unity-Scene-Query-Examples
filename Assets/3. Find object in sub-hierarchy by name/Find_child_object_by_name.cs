aaea9ca2e3db4c001b22281e1470e57bdde6eb31
Ashley Davis <ashley.davis@realseriousgames.com>
2016-08-08 16:44:35 +1000

    Example 3.

---
+++ Assets/3. Find object in sub-hierarchy by name/Find_child_object_by_name.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Find_child_object_by_name : MonoBehaviour
+{
+	void Start ()
+    {
+        var parent = GameObject.Find("Parent");
+        var child = parent.transform.FindChild("Child");
+        Debug.Log("Found child object: " + child.name);
+    }
+}
