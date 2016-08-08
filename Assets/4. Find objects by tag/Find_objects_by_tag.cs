45a676245a605e8c5e29de24f3e5b3e8e3a88238
Ashley Davis <ashley.davis@realseriousgames.com>
2016-08-08 16:59:47 +1000

    Example 4.

---
+++ Assets/4. Find objects by tag/Find_objects_by_tag.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Find_objects_by_tag : MonoBehaviour
+{
+	void Start ()
+    {
+	    var pedestrians = GameObject.FindGameObjectsWithTag("Pedestrian");
+        foreach (var pedestrian in pedestrians)
+        {
+            Debug.Log("Found pedestrian: " + pedestrian.name);
+        }
+    }
+}
