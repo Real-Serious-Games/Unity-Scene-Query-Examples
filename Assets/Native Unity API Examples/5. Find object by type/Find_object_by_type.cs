ae62b55899f5e4756de56504a0404381ecc5e06f
Ashley Davis <ashley.davis@realseriousgames.com>
2016-08-08 17:08:01 +1000

    Example 5.

---
+++ Assets/Native Unity API Examples/5. Find object by type/Find_object_by_type.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Find_object_by_type : MonoBehaviour
+{
+	void Start ()
+    {
+        var leaderboard = GameObject.FindObjectOfType<Leaderboard>();
+        Debug.Log("Found leaderboard: " + leaderboard.name);	
+	}
+}
