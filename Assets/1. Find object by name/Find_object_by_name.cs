98c3ae7dff79eeeb0e68f402fe6ed207f3574de8
Ashley Davis <ashley.davis@realseriousgames.com>
2016-08-08 16:31:08 +1000

    Example 1.

---
+++ Assets/1. Find object by name/Find_object_by_name.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Find_object_by_name : MonoBehaviour {
+
+	void Start ()
+    {
+        var player = GameObject.Find("Player");
+        Debug.Log("Found game object " + player.name);
+	}
+}
