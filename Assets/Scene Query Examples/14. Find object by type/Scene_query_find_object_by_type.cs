fb077cbd882633e41199e4403cbdd588f4d1a4c4
Ashley Davis <ashley.davis@realseriousgames.com>
2016-08-11 10:27:11 +1000

    Added examples 14 and 15.

---
+++ Assets/Scene Query Examples/14. Find object by type/Scene_query_find_object_by_type.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using RSG.Scene.Query;
+
+public class Scene_query_find_object_by_type : MonoBehaviour
+{
+	void Start ()
+    {
+        var sceneQuery = new SceneQuery();
+
+        var player = sceneQuery.SelectOne<Player>();
+        Debug.Log("Found player: " + player.name);
+    }
+}
---
+++ Assets/Scene Query Examples/15. Find objects by type/Scene_query_find_objects_by_type.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using RSG.Scene.Query;
+
+public class Scene_query_find_objects_by_type : MonoBehaviour
+{
+	void Start ()
+    {
+        var sceneQuery = new SceneQuery();
+
+        var pedestrians = sceneQuery.SelectAll<Pedestrian>();
+        foreach (var pedestrian in pedestrians)
+        {
+            Debug.Log("Found pedestrian: " + pedestrian.name);
+        }
+    }
+}
